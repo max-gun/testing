5faae9c4cc62ba24a3fa4cf523147d7718651368
Max Gun <max.gun@playstudios-il.com>
2024-08-22 13:50:26 +0300

    second commit

---
+++ src/main/java/com/enterprise/testings/TestingsApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.testings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enterprise/testings/TestingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.testings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
