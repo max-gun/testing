e2ef1374d1e5371ab4e7a4bfb3ebefefaf4275d5
Max Gun <max.gun@playstudios-il.com>
2024-09-02 19:51:28 +0300

    adding Exchanger

---
+++ src/main/java/com/enterprise/testings/util/Exchanger.java
@@ -0,0 +1,20 @@
+package com.enterprise.testings.util;
+
+/**
+ * @author max.gun
+ * @since 02/09/2024
+ */
+public class Exchanger {
+
+    public static void foo1() {
+        System.out.println("foo1");
+    }
+
+    public static void foo2() {
+        System.out.println("foo2");
+    }
+
+    public static void foo3() {
+        System.out.println("foo3");
+    }
+}
