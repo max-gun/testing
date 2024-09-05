eff0c9c2aee0c2b2ca1c38762d5a99b5c521c308
Max Gun <max.gun@playstudios-il.com>
2024-09-05 19:04:16 +0300

    more regex

--- src/main/java/com/enterprise/testings/util/RegexExample.java
+++ src/main/java/com/enterprise/testings/util/RegexExample.java
@@ -8,20 +8,54 @@ import java.util.regex.Pattern;
  * @since 05/09/2024
  */
 public class RegexExample {
+
+    public static boolean isValidEmail(String emailTarget) {
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(emailTarget);
+        return matcher.find();
+    }
+
     public static void main(String[] args) {
-        String regex = "[^\\d]+";
+        String email = "max.gun17@gmail.com";
+        boolean isValid = isValidEmail(email);
+        System.out.println("Result: " + isValid);
+
+        /**
+         * input: "1?-12" => output: "12-12"
+         * input: "?4-?1" => output: "04-21"
+         * input: "?3-?1" => output: "03-31"
+         * input: "02-3?" => output: "02-19"
+         * input: "??-??" => output: "12-31"
+         * input: "5?-??" => output: "xx-xx"
+         *
+         * "MM-DD"
+         * "?M-DD"
+         * "M?-DD"
+         * "??-DD"
+         * "MM-?D"
+         * "?M-?D"
+         * "M?-?D"
+         * "??-?D"
+         * "MM-D?"
+         * "?M-D?"
+         * "M?-D?"
+         * "??-D?"
+         * "MM-??"
+         * "?M-??"
+         * "M?-??"
+         * "??-??"
+         *
+         * */
+
+        /*String regex = "[^\\d]+";
         Pattern pattern = Pattern.compile(regex);
 
-        String target = "There are 26 Shishlik, so put it on the Mangal";
+        String target = "There are 26 Shishlik and 153 Kebabs";
         Matcher matcher = pattern.matcher(target);
 
-        boolean isExist = matcher.find();
-
-        if (isExist) {
+        while (matcher.find()) {
             System.out.println("RESULT: " + matcher.group());
-        }
-        else {
-            System.out.println("Ain't found sh**!!");
-        }
+        }*/
     }
 }
